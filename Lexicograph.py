1d30a2538444215e1fa8c5c12c3ea22e4f1c5500
Alec Vis <visalec058@gmail.com>
2021-03-09 08:05:49 -0800

    completed skew function and created the graph for the e_coli genome data

--- Lexicograph.py
+++ Lexicograph.py
@@ -102,6 +102,43 @@ def clump_finding(genome, k, t, l):
     return freq_pat
 
 
+def skew(genome):
+    """ the skew plot takes adavntage of the fact that DNA polymerase only replicates in the 3' to 5' (reverse half strand)
+     direction.Therefore, Okazaki fragments are created in the 5' to 3' direction and the single stranded DNA needs to
+    'hangout' for a period of time making it more susceptible to mutation. the nucleotide that is extremely susceptible
+    is Cytosine. Thus on the forward half strand, there is a decrease in the amount of Cytosine as it mutates into Thymine.
+    The repair mechanisms compound this by repairing Guanine into Adenine on the reverse half strand.
+
+    In conclusion this implies that there will be a high C:G ratio in the reverse half strand and a low C:G ratio in the
+    forward half strand. So if we walk along the DNA strand keeping track of this C:G ratio and we see a change from
+    low to high then we know the replication origin is at that location"""
+    c_count = 0
+    g_count = 0
+    skw = []
+    genome = genome.lower()
+    for i in genome:
+        if i == 'c':
+            c_count += 1
+        elif i == 'g':
+            g_count += 1
+        skw.append(c_count - g_count)
+    return skw
+
+
+# Generate the line plot for the skew
+""" note this can take a few minutes to generate
+for e coli the image is saved as a .png
+Note the V shape of the graph with a minimum as ~3e6 base pairs
+This is our hypothesized origin of replication"""
+# print('calcuating skew')
+# skw = skew(seq)
+# print('skew calculation completed')
+# print('generating line plot object')
+# sns.lineplot(x=np.arange(len(seq)), y=skw)
+# print('rendering object')
+# plt.show()
+
+
 if __name__ == '__main__':
     data1 = pd.read_csv(r'C:\Users\Alec Vis\AVBioInfo\data\rosalind_ba1b.txt')
     data2 = " AGGATAATTGATATGATTCAGTTGATATGTCTGATATGGATATGATTGATATGATGATATGCCACTGATATGTTGATATGTTGATATGGCTGATATGTAGCATTTTGATATGGTGATATGTGATATGCCTTGATATGGGGTTGATATGCTGATATGTTTTCATGATATGATGATATGTGATATGCTACTACTGATATGTGATGATATGTGTGATATGCTGATTGATATGGAGGTGATATGTTTTACTATGATATGCCCTATTTGATATGTGATATGTGATATGTGATATGTGTGCTGATATGGATTCGCTGATATGGCTGATATGTTGATATGATGATATGGATCTGTCCTATCTTGATATGCATGATATGAAATGATATGGTGATATGTCAAGATGATATGGTGATATGTGATATGCCATGATATGATGATATGGTGATATGCTGATATGTGATATGGGTGATATGGCTGTGATATGACACCCCCATCGATATGATATGTGATATGGTCCTTGATATGATGATATGTCGATGATATGTGATATGTAGATAGTGATATGTGATATGTTATGATATGCCTGATATGCAGTTGATATGTTGATATGTGATATGATTGATATGCTGATATGTGATATGCGGTCTGATATGCGCGTAGCAGTGATATGTGATATGATTATGATATGTGATATGTGATATGCTGATATGTGATATGTGGTAGCAGTGATATGGTTCGGGTGATATGTGATATGTGATATGGCTGATATGTGATATGGAATCATCTCTGTGAATGATATGACAGCTCGTGATATGTGATATGGCCCTGATATGGCTGATATGTGATATGTTGATATGCCTGATATGTATGATATGCTCCATCTGATATGTGATATGTGATATGTGATATGTGATATGTGATATGGGTCTGATATGTTGATATGTACTGATATGTGATATGGTGATATGTATGATATG"
